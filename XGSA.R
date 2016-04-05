15423b3a5ce0074ba2eea5434455fdd43c3046d3
djevic <djdjordjevic@hotmail.com>
2016-04-05 11:37:53 +1000

    Copyright and License information

--- XGSA.R
+++ XGSA.R
@@ -1,5 +1,19 @@
 # XGSA.R
-# Djordje Djordjevic 2016
+# Copyright Djordje Djordjevic 2016
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
 ##########################
 #
 # This file contains the neccessary R code to run XGSA as described in "XGSA: an unbiased statistical method for cross-species gene set analysis"
@@ -27,6 +41,7 @@
 # biocLite("biomaRt")
 # install.packages("Matrix")
 # 
+##########################
 
 require(Matrix)
 require(biomaRt)
